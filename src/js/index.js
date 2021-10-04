9b626bbaf8670ed5932907b0ab5f8712dd973f24
Margaret <match14@yandex.ru>
2021-10-05 01:50:35 +0300

    part 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,36 @@
 import {$} from './common';
 
-$(window).scroll(function(){
-	if($(this).scrollTop()>300){
-		$('.js-move-up').addClass('visible');
-	}else{
-		$('.js-move-up').removeClass('visible');
-	}
-});
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+// Слайдер клиентов
+if($('.js-gallery-slider').length){
+	$('.js-gallery-slider').slick({
+		slidesToShow: 5,
+		// slidesToScroll: 5,
+		infinite: true,
+		centerMode: true,
+		centerPadding: '0px',
+		swipe:false,
+		dots: true,
+		appendDots: $('.js-gallery-slider-dots'),
+		prevArrow: '<button id="prev" type="button" class="btn-slider btn-slider_left"><svg class="icon ic-arrow-left" width="40" height="35"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-left"></use></svg></button>',
+		nextArrow: '<button id="next" type="button" class="btn-slider btn-slider_right"><svg class="icon ic-arrow-right" width="40" height="35"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-right"></use></svg></button>',
+		// responsive: [
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow: 1.5,
+		// 			slidesToScroll: 1,
+		// 		}
+		// 	},
+		// ]
+	});
+
+	$('.js-gallery-slider').find('.slick-slide.slick-active').each(function(indx, element){
+		$(element).attr('data-active',indx+1);
+	});
+
+	$('.js-gallery-slider').find('.slick-arrow').on('click', function(){
+		$('.js-gallery-slider').find('.slick-slide.slick-active').each(function(indx, element){
+			$(element).attr('data-active',indx+1);
+		});
+	});
+}
